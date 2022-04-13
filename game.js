9e1912de7290dfbfa3676ad2c3e3d2321cc792c6
Arthur Ghazaryan <newartroll11@gmail.com>
2022-04-13 18:51:16 +0400

    feat:(platform area player standic work)

--- game.js
+++ game.js
@@ -35,7 +35,24 @@ class Player {
   }
 }
 
+class Platform {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 100,
+    };
+    this.width = 200;
+    this.height = 20;
+  }
+  draw() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+const platform = new Platform();
+
 const keys = {
   right: {
     pressed: false,
@@ -49,12 +66,25 @@ function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platform.draw();
+  // left right jump
   if (keys.left.pressed) {
     player.velocity.x = -5;
   } else if (keys.right.pressed) {
     player.velocity.x = 5;
   } else {
     player.velocity.x = 0;
+  }
+
+  //   platformWork
+  if (
+    player.position.y + player.height <= platform.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform.position.y &&
+    player.position.x + player.width >= platform.position.x &&
+    player.position.x <= platform.position.x + platform.width
+  ) {
+    player.velocity.y = 0;
   }
 }
 animate();
@@ -77,12 +107,12 @@ window.addEventListener("keydown", ({ key }) => {
       break;
     case "w":
       console.log("up");
-      if (player.velocity.y < 1 && player.velocity.y > -1) {
-        player.velocity.y -= 20;
-        break;
-      } else {
-        break;
-      }
+      //   if (player.velocity.y < 1 && player.velocity.y > -1) {
+      player.velocity.y -= 20;
+      //     break;
+      //   } else {
+      break;
+    //   }
     case "s":
       console.log("down");
       if (player.velocity.y >= 0) {
