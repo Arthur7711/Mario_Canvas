0bac96c2133e8c7a0a43d8a0300302d1ce35ee54
Arthur Ghazaryan <newartroll11@gmail.com>
2022-04-13 18:27:32 +0400

    feat:(player movement)

--- game.js
+++ game.js
@@ -26,6 +26,7 @@ class Player {
   update() {
     this.draw();
     this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity;
     } else {
@@ -35,10 +36,93 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 animate();
+
+// eventListeners
+addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+window.addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      console.log("left");
+      keys.left.pressed = true;
+      break;
+    case "d":
+      console.log("right");
+      keys.right.pressed = true;
+      break;
+    case "w":
+      console.log("up");
+      if (player.velocity.y < 1 && player.velocity.y > -1) {
+        player.velocity.y -= 20;
+        break;
+      } else {
+        break;
+      }
+    case "s":
+      console.log("down");
+      if (player.velocity.y >= 0) {
+        break;
+      } else {
+        player.velocity.y += 20;
+        break;
+      }
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+    case "d":
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+    case "w":
+      console.log("up");
+      if (player.velocity.y < 1 && player.velocity.y > -1) {
+        player.velocity.y -= 20;
+        break;
+      } else {
+        break;
+      }
+    case "s":
+      console.log("down");
+      if (player.velocity.y >= 0) {
+        break;
+      } else {
+        player.velocity.y += 20;
+        break;
+      }
+    default:
+      break;
+  }
+});
