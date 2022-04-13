bc2401635aca13096ef0afac9162fce3f14a2ea9
Arthur Ghazaryan <newartroll11@gmail.com>
2022-04-13 16:21:03 +0400

    feat:(Player creation)

---
+++ game.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    ctx.fillStyle="red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const player = new Player();
+player.draw();
